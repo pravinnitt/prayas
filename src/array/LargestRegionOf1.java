db757dfab27a163d9253d830280525025a603491
Pravin Kumar <pravin.kumar02@sap.com>
2019-08-11 14:20:25 +0530

    changes

---
+++ src/array/LargestRegionOf1.java
@@ -0,0 +1,203 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * find Unit Area of largest region of 1's
+ *
+ *  1 1 0
+ *  0 1 0
+ *  1 0 1
+ *  0 0 1
+ */
+public class LargestRegionOf1 {
+
+
+    public static int[][] input ={{1,1,0},{0,1,0},{1,0,1},{0,0,1}};
+    public static void main(String[] args){
+
+        LargestRegionOf1 lregion = new LargestRegionOf1();
+
+        UnitCell[][] store = lregion.transformMatrixToCell(input);
+        lregion.printInput(store);
+        UnitCell one = store[0][1];
+        System.out.println(" Maximum region length  = " + lregion.findMaxRegion(store));
+    }
+
+    public int findMaxRegion(UnitCell[][] param) {
+        int maxLength = 0;
+        int rowSize = param.length;
+        int columnSize = param[0].length;
+        for (int i = 0; i < rowSize; i++) {
+            int curLength = 0;
+            for (int j = 0; j < columnSize; j++) {
+                Queue<UnitCell> list = new LinkedList<>();
+                if(param[i][j].getValue()==1 && !param[i][j].isVisited) {
+                    list.add(param[i][j]);
+                }
+                while (!list.isEmpty())
+                {
+                    UnitCell cur = list.poll();
+                    if(!cur.isVisited) {
+                        List<UnitCell> neighbours = findPopulatedNeighbour(cur, param);
+                        for(UnitCell nbr:neighbours)
+                        {
+                            if(!nbr.isVisited){
+                                list.add(nbr);
+                            }
+                        }
+                        curLength+=1;
+                        if(curLength>maxLength) {
+                            maxLength=curLength;
+                        }
+                        cur.setVisited(true);
+                    }
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
+    /**
+     *
+     *          (i-1,j-1)      (i-1,j) (i-1,j+1)
+     *          (i,j-1)        (i,j)   (i,j+1)
+     *          (i+1,j-1)      (i+1,j) (i+1,j+1)
+     *
+     * @param cell
+     */
+    private List<UnitCell> findPopulatedNeighbour(UnitCell cell,UnitCell[][] complete)
+    {
+        List<UnitCell> neighbours = new ArrayList<>();
+        int rowsize = complete.length;
+        int colsize = complete[0].length;
+
+        int xcord = cell.getXcord();
+        int ycord = cell.getYcord();
+        if(xcord !=0){ //previous row can be considerd
+            UnitCell above = complete[xcord-1][ycord];
+            if(above.getValue()==1) {
+                neighbours.add(above);
+            }
+            if(ycord!=0) {
+                UnitCell aboveLeft = complete[xcord-1][ycord-1];
+                if(aboveLeft.getValue()==1) {
+                    neighbours.add(aboveLeft);
+                }
+            }
+            if(ycord!=colsize-1) {
+                UnitCell aboveRight = complete[xcord-1][ycord+1];
+                if(aboveRight.getValue()==1) {
+                    neighbours.add(aboveRight);
+                }
+            }
+        }
+        if(xcord < rowsize-1) {// Next row can be considerd
+            UnitCell below = complete[xcord+1][ycord];
+            if(below.getValue()==1) {
+                neighbours.add(below);
+            }
+            if(ycord!=0) {
+                UnitCell belowLeft = complete[xcord+1][ycord-1];
+                if(belowLeft.getValue()==1) {
+                    neighbours.add(belowLeft);
+                }
+            }
+            if(ycord!=colsize-1) {
+                UnitCell belowRight = complete[xcord+1][ycord+1];
+                if(belowRight.getValue()==1) {
+                    neighbours.add(belowRight);
+                }
+            }
+
+        }
+        if(ycord!=0) {
+            UnitCell Left = complete[xcord][ycord-1];
+            if(Left.getValue()==1) {
+                neighbours.add(Left);
+            }
+        }
+        if(ycord!=colsize-1) {
+            UnitCell Right = complete[xcord][ycord+1];
+            if(Right.getValue()==1) {
+                neighbours.add(Right);
+            }
+        }
+        return neighbours;
+    }
+
+
+    public UnitCell[][] transformMatrixToCell(int[][] param) {
+        int rowSize = param.length;
+        int columnSize = param[0].length;
+        UnitCell[][] temp = new UnitCell[rowSize][columnSize];
+
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < columnSize; j++) {
+                UnitCell cell = new UnitCell(i,j,param[i][j]);
+                temp[i][j] =cell;
+            }
+            System.out.println();
+        }
+
+        return  temp;
+    }
+    public void printInput(UnitCell[][] param) {
+        int rowSize = param.length;
+        int columnSize = param[0].length;
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j <columnSize ; j++) {
+                System.out.print(" " +param[i][j].getValue());
+            }
+            System.out.println();
+        }
+    }
+}
+
+ class UnitCell {
+     public int getXcord() {
+         return xcord;
+     }
+
+     public void setXcord(int xcord) {
+         this.xcord = xcord;
+     }
+
+     public int getYcord() {
+         return Ycord;
+     }
+
+     public void setYcord(int ycord) {
+         Ycord = ycord;
+     }
+
+     public int getValue() {
+         return value;
+     }
+
+     public void setValue(int value) {
+         this.value = value;
+     }
+
+     public boolean isVisited() {
+         return isVisited;
+     }
+
+     public void setVisited(boolean visited) {
+         isVisited = visited;
+     }
+
+     int xcord, Ycord,value;
+    boolean isVisited;
+
+     UnitCell(int x, int y, int val) {
+         this.xcord =x;
+         this.Ycord =y;
+         this.value =val;
+         this.isVisited =false;
+     }
+}
