d1ae11f752a23234415d1719b751e5d22bad725c
Pravin Kumar <pravin.kumar02@sap.com>
2019-08-11 14:22:52 +0530

    changes

---
+++ src/AutoComplete/AutoComplete.java
@@ -0,0 +1,10 @@
+package AutoComplete;
+
+public class AutoComplete {
+
+    int resultSize =3;
+
+    public static void main(String[] args) {
+        System.out.println(AutoComplete.class.getName());
+    }
+}
